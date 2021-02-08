28c2af86200a502f46c8efc2573f6fe250180bc4
Debosmit Neogi <59002072+Debosmit-Neogi@users.noreply.github.com>
2021-02-08 11:05:00 +0530

    Create trans.h

---
+++ test/trans.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define n 4
+#define m 4
+
+void transpose(int A[n][m], int B[n][m]) 
+{ 
+    int i, j; 
+    for (i = 0; i < n; i++) 
+        for (j = 0; j < m; j++) 
+            B[i][j] = A[j][i]; 
+} 
